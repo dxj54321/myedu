ad68becffdcb205545754fabb1f9dc92b3bdb00a
豆贤俊 <1197465564@qq.com>
2019-04-19 12:00:32 +0800

    新增list查询

---
+++ day02/list_type.py
@@ -0,0 +1,13 @@
+
+
+def list_demo(a):
+    print(a[4])
+    print(a[1:3])
+    print(a[-2])
+    print(a[-4:-2])
+    print(a[0:1])
+    print(a[-1:-2])
+
+if __name__ == '__main__':
+    alist = ['a',4,'xiaoniao','80','1.36','中国']
+    list_demo(alist)
